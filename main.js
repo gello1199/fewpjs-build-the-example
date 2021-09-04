a9cd740567a3738ef2c16fee475a2a61197ff1ad
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-09-03 23:11:16 -0400

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,42 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+//run mimic on the like click
+// if mimic service call is resolved, click gives us full heart
+// if not resolved throw error message
+// mimicServerCall()
 
+let glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
+
+let colorStates = {
+  "red" : "",
+  "": "red"
+};
+
+let heartImage = document.querySelectorAll(`.like-glyph`);
+
+function likeButton(e) {
+  let heart = e.target;
+  mimicServerCall(`url`)
+  .then(function(serverMessage){
+    heart.innerText = glyphStates[heart.innerText];
+    heart.style.color = colorStates[heart.style.color];
+  })
+  .catch(function(error) {
+    alert(error.message);
+    let errorMessage = document.getElementById(`modal`)
+    errorMessage.style.visibility = `visible`;
+    setTimeout(function() {
+      errorMessage.style.visibility = `hidden`;
+    }, 5000);
+  });
+}
+  for(let glyph of heartImage) {
+    glyph.addEventListener(`click`, likeButton);
+  }
 
 
 
